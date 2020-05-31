d8e0c2a5788dd6c6276ebcb2592fd46b47a27d17
Kris Chavez <chavez.kris3@gmail.com>
2020-05-31 18:17:51 +1000

    changed to response values

--- app/src/main/java/com/example/speechrecognition/MainActivity.java
+++ app/src/main/java/com/example/speechrecognition/MainActivity.java
@@ -1,48 +1,68 @@
 package com.example.speechrecognition;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.icu.text.StringSearch;
+import android.media.tv.TvContract;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import org.w3c.dom.Text;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
     //views from activity
     TextView mTextTv;
+    TextView mTextResponses;
     ImageButton mVoiceBtn;
+    List<String> wordBank = new ArrayList<>();
+    List<String> responses = new ArrayList<>();
+
+    boolean word = false;
 
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextTv = findViewById (R.id.textTv);
-        mVoiceBtn = findViewById(R.id.voiceBtn);
+        mTextTv = (TextView) findViewById(R.id.textTv);
+        mTextResponses = (TextView) findViewById(R.id.textTv);
+        mVoiceBtn = (ImageButton) findViewById(R.id.voiceBtn);
+        wordBank = Arrays.asList(getResources().getStringArray(R.array.Words));
+        responses = Arrays.asList(getResources().getStringArray(R.array.responses));
 
         //button click to show speech to text dialog
-        mVoiceBtn.setOnClickListener (new View.OnClickListener() {
-         @Override
-         public void onClick (View v) {
-             speak();
+        mVoiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
             }
-         });
+        });
     }
 
-    private void speak() {
+    public void speak() {
         //intent to show speech to text dialog
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak something");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi say something");
 
         //start intent
         try {
@@ -54,26 +74,22 @@ public class MainActivity extends AppCompatActivity {
             //get message of error and show
             Toast.makeText(this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-
     }
 
-        //receive voice input and handle it
-        protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
 
-            switch (requestCode) {
-                case REQUEST_CODE_SPEECH_INPUT:{
-                    if(resultCode == RESULT_OK && null!=data) {
-                        //get text array from voice intent
-                        ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                        //set to text view
-                        mTextTv.setText(result.get(0));
-                    }
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        int responseItem =0;
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT && resultCode == RESULT_OK) {
+            ArrayList<String> result = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            for(String wordItem: wordBank) {
+                if (result.get(0).contains(wordItem)) {
+                    mTextTv.setText(result.get(0).toUpperCase());
+                    mTextResponses.setText(responses.get(responseItem).toString());
                     break;
                 }
+                responseItem++;
             }
-
         }
-
     }
-
+}
